49b07ff76fa347a2c067768c0a66f76849d085a3
Saurabh Khade <khadesaurabh056@gmail.com>
2021-04-24 20:54:28 +0530

    initial setup

---
+++ components/Head.js
@@ -0,0 +1,22 @@
+import NextHead from 'next/head'
+
+export default function Head(props) {
+  <NextHead>
+    <title>{props.title}</title>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="description" content={props.desc} />
+    <meta name="robots" content="index, follow" />
+    <meta property="og:type" content="Math Calculator" />
+    <meta property="og:title" content={props.title} />
+    <meta property="og:description" content={props.desc} />
+    <meta property="og:image" content="/img/logo.webp" />
+    <meta property="og:url" content="https://saurabhkhade.github.io/Math-Calculator" />
+    <meta property="og:site_name" content="Math-Calculator" />
+    <meta name="keywords" content={`maths,calculator,saurabh khade,${props.keys}`}>
+    <meta name="author" content="Saurabh Khade">
+    
+    <link rel="shortcut icon" href="/img/logo.ico" type="image/x-icon" />
+  </NextHead>
+}
---
+++ components/functions.js
@@ -0,0 +1,21 @@
+function factorial(number) {
+  let result=1
+  for(let i=1; i<=number; i++) {
+    result*=i
+  }
+  return result
+}
+
+function combinations(n,r) {
+  let nFact = factorial(n)
+  let rFact = factorial(r)
+  let nrFact = factorial(n-r)
+  return (nFact/(rFact*nrFact))
+}
+
+export function binomialProbability(n,r,p) {
+  let combs = combinations(n,r)
+  let success = Math.pow(p,r)
+  let failure = Math.pow(1-p,n-r)
+  return (combs*success*failure).toFixed(6)
+}
